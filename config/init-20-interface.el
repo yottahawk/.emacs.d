5dfe3232e9fef36b3ef8a458245ffa8e5c0f7bf8
Harry Callahan <unsuitableduck@gmail.com>
2018-04-06 17:29:47 +0100

    Added more keybinds to be global and un-overrideable.

--- config/init-20-interface.el
+++ config/init-20-interface.el
@@ -16,9 +16,6 @@
 (use-package undo-tree)
 ;;(use-package browse-kill-ring+)
 
-(global-set-key (kbd "C-j") 'previous-buffer)
-(global-set-key (kbd "C-k") 'next-buffer)
-
 ;; Start emacs server mode, to allow ActiveHDL to open files inside an active window
 (server-start)
 
@@ -27,6 +24,12 @@
 
 ;; User ranger mode instead of dired by default
 (ranger-override-dired-mode t)
+
+;; Set bind-key global keybinds that cannot be overwritten by major-modes.
+(bind-keys*
+ ("C-k" . next-buffer)
+ ("C-j" . previous-buffer)
+ ("C-z" . undo))
 
 ;; Function to temporarily maximise a buffer
 (defun toggle-maximize-buffer () "Maximize buffer"
@@ -38,4 +41,4 @@
       (delete-other-windows))))
 
 (global-set-key (kbd "<C-f1>") 'toggle-maximize-buffer)
-(global-set-key (kbd "C-z") 'undo)
+
