2a6e31624691a4984c0a3b606cc9d465c1a7475f
Harry Callahan <hcalla93@gmail.com>
2018-05-06 18:40:56 +0100

    org mode changes

--- config/init-31-doc-org.el
+++ config/init-31-doc-org.el
@@ -6,13 +6,12 @@
 (define-key global-map "\C-ca" 'org-agenda)
 
 ;; Install org-wiki from Github if it is not already installed.
-(if (require 'org-wiki)
-    (print "Already installed!")   
-  ((let ((url "https://raw.githubusercontent.com/caiorss/org-wiki/master/org-wiki.el"))     
-     (with-current-buffer (url-retrieve-synchronously url)
-       (goto-char (point-min))
-       (re-search-forward "^$")
-       (delete-region (point) (point-min))
-       (kill-whole-line)
-       (package-install-from-buffer)))))
-
+(if (require 'org-wiki nil 'noerror)
+    (print "Already installed!")
+  (let ((url "https://raw.githubusercontent.com/caiorss/org-wiki/master/org-wiki.el"))
+      (with-current-buffer (url-retrieve-synchronously url)
+	(goto-char (point-min))
+	(re-search-forward "^$")
+	(delete-region (point) (point-min))
+	(kill-whole-line)
+	(package-install-from-buffer))))
